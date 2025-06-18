3ebfc0ba72884f0cd290f71f0a10666715fce033
Gregg3301 <deivid.souza2828@gmail.com>
2025-06-18 02:01:12 +0000

    desenvolvido o sistema de demonstacao de movimento das pecas de xadrez

--- xadrez.c
+++ xadrez.c
@@ -16,7 +16,7 @@ int main() {
 
     do
     {
-        printf("=== Bem-vindo à demonstração de movimentação de peças do jogo de xadrez. ===\nEscolha qual peça deseja ver a demonstração.\n");
+        printf("\n=== Bem-vindo à demonstração de movimentação de peças do jogo de xadrez. ===\nEscolha qual peça deseja ver a demonstração.\n");
         printf("1 -> Bisbo\n");
         printf("2 -> Rainha\n");
         printf("3 -> Torre\n");
@@ -27,35 +27,61 @@ int main() {
         switch (option)
         {
         case 1:
-            strcpy(piece, "Bispo");
+
+            strcpy(piece, "Bispo");       
             strcpy(direction1, "Cima");
             strcpy(direction2, "Direita");
             position = 5;
 
-            printf("Você escolheu o %s!\nMovendo\n", piece);
+            printf("\nVocê escolheu a peça %s!\nMovendo..\n\n", piece);
 
-            for (int i = 0; i <= position; i++)
+            for (int i = 0; i < position; i++)
             {
                 printf("%s \n%s \n", direction1, direction2);
             }
 
             break;
         case 2:
-            /* code */
+            strcpy(piece, "Rainha");       
+            strcpy(direction1, "Esquerda");
+            position = 8;
+
+            printf("\nVocê escolheu a peça %s!\nMovendo..\n\n", piece);
+
+            while (position > 0)
+            {
+                printf("%s\n", direction1);
+                position--;
+            }
+            
             break;
         case 3:
-            /* code */
+            strcpy(piece, "Torre");       
+            strcpy(direction1, "Cima");
+            position = 5;
+
+            printf("\nVocê escolheu a peça %s!\nMovendo..\n\n", piece);
+
+            do
+            {
+                printf("%s\n", direction1);
+                position--;
+            } while (position > 0);
+            
+
             break;
-        
+        case 0:
+            printf("Saindo do programa, até logo!\n2");
+            return 0;
+            break;
         default:
+            printf("Opcao Invalida!\n");
             break;
         }
     } while (option != 0);
     
     
     
-    
-
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
